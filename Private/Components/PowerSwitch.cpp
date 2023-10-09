b71eafae2a6e070e569eb2970d513458bdb3df4f
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-10-09 14:29:09 +0100

    Everything in the constructor was made redundant, so is deleted. APowerSwitch no longer needs to know that Equipment exists, so #include "Equipment/Switchable.h" was removed, which extends this class to be used outside of equipment usage. For example, this switch can now be used as a light switch in a house. Instead of directly calling a parents Toggle functions, ToggleSwitch() now broadcasts that the switch has been triggered.

--- Private/Components/PowerSwitch.cpp
+++ Private/Components/PowerSwitch.cpp
@@ -1,14 +1,5 @@
 #include "Components/PowerSwitch.h"
 
-#include "Equipment/Switchable.h"
-
-UPowerSwitch::UPowerSwitch()
-{
-	PrimaryComponentTick.bCanEverTick = false;
-
-	Parent = Cast<ASwitchable>(GetOwner());
-}
-
 bool UPowerSwitch::IsTurnedOn() const
 {
 	return bPoweredOn;
@@ -18,8 +9,5 @@ void UPowerSwitch::ToggleSwitch()
 {
 	bPoweredOn = !bPoweredOn;
 
-	if (Parent)
-	{
-		this->IsTurnedOn() ? Parent->TurnedOn() : Parent->TurnedOff();
-	}
+	SwitchToggled.Broadcast(this->IsTurnedOn());
 }
