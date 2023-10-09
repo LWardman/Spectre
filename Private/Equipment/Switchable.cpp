828e925565223f8c8ce0de1b22ca70f4033c02e7
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-10-09 14:32:30 +0100

    Added Logs for debugging whether the players current equipment has been turned on or off, just for debugging purposes. In the constructor, ToggleEquipment() has been set to listen to the PowerSwitch. ToggleEquipment() has been implemented, similar to how the PowerSwitch used to tell the ewquipment if it was on or off.

--- Private/Equipment/Switchable.cpp
+++ Private/Equipment/Switchable.cpp
@@ -8,14 +8,24 @@ ASwitchable::ASwitchable()
 	PrimaryActorTick.bCanEverTick = true;
 
 	PowerSwitch = CreateDefaultSubobject<UPowerSwitch>(TEXT("Power Switch"));
+
+	if (PowerSwitch)
+	{
+		PowerSwitch->SwitchToggled.AddDynamic(this, &ASwitchable::ToggleEquipment);
+	}
+}
+
+void ASwitchable::ToggleEquipment(bool bEquipmentTurnedOn)
+{
+	bEquipmentTurnedOn ? TurnedOn() : TurnedOff();
 }
 
 void ASwitchable::TurnedOn()
 {
-	
+	UE_LOG(LogTemp, Display, TEXT("Turned On"));
 }
 
 void ASwitchable::TurnedOff()
 {
-	
+	UE_LOG(LogTemp, Display, TEXT("Turned Off"));
 }
