97cd33a0db7b36ff48e2e1f239c4a093a8d98d43
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-11-02 12:05:51 +0000

    - Added basic stats and methods of changing these stats.

--- Public/Characters/Ghost.h
+++ Public/Characters/Ghost.h
@@ -19,4 +19,22 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
+	// These stats will be used on a scale of 0 - 10, 5 being default.
+	
+	int32 Aggressiveness = 5;	// Determines how much the ghost will attack or try to discourage the player.
+	
+	int32 Interactiveness = 5;	// Determines how often the ghost will initiate events with the environment and equipment.
+	
+	int32 Curiosity = 5;		// Determines how much the ghost will follow the player / change rooms.
+
+
+	float Speed = 550.f;
+	
+	
+	void IncreaseStat(int32& Stat, int32 Increment);
+
+	void DecreaseStat(int32& Stat, int32 Decrement);
+
+	void LogStats() const; // Debug function
+
 };
