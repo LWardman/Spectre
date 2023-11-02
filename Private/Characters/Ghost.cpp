090124f7aa02698719e0024c6c37e7148272d4b9
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-11-02 12:06:17 +0000

    - Defined methods of increasing and decreasing stats

--- Private/Characters/Ghost.cpp
+++ Private/Characters/Ghost.cpp
@@ -4,18 +4,41 @@
 AGhost::AGhost()
 {
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 void AGhost::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void AGhost::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void AGhost::IncreaseStat(int32& Stat, int32 Increment)
+{
+	Stat += Increment;
+	
+	if (Stat >= 10)
+	{
+		Stat = 10;
+	}
+}
+
+void AGhost::DecreaseStat(int32& Stat, int32 Decrement)
+{
+	Stat -= Decrement;
+	
+	if (Stat <= 0)
+	{
+		Stat = 0;
+	}
+}
+
+void AGhost::LogStats() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Aggressiveness : %i"), Aggressiveness);
+	UE_LOG(LogTemp, Warning, TEXT("Interactiveness : %i"), Interactiveness);
+	UE_LOG(LogTemp, Warning, TEXT("Curiosity : %i"), Curiosity);
+}
