1f2cbfd0ec24b899663b7c941e73e315f868260c
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-11-16 14:05:53 +0000

    - Defined SpawnEMFEvent
    - Included EMF sphere header

--- Private/Components/Ghost/EMFGenerator.cpp
+++ Private/Components/Ghost/EMFGenerator.cpp
@@ -1,20 +1,31 @@
 
 #include "Components/Ghost/EMFGenerator.h"
 
+#include "Actors/EMFSphere.h"
+
+
 UEMFGenerator::UEMFGenerator()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 }
-
 
 void UEMFGenerator::BeginPlay()
 {
 	Super::BeginPlay();
 }
 
-
 void UEMFGenerator::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
+void UEMFGenerator::SpawnEMFEvent(int32 Level, float Radius, FVector Center)
+{
+	AActor* EMF_Sphere = GetWorld()->SpawnActor(AEMFSphere::StaticClass());
+
+	if (AEMFSphere* EMF = Cast<AEMFSphere>(EMF_Sphere))
+	{
+		EMF->Initialise(Level, Radius, Center);
+		EMF->SpawnEMF();
+	}
+}
