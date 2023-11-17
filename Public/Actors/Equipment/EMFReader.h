c9a099d9cd7a2e3633811e711e2bbee8b728d888
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-11-17 13:01:46 +0000

    Initial Commit

---
+++ Public/Actors/Equipment/EMFReader.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Equipment/Switchable.h"
+#include "EMFReader.generated.h"
+
+
+UCLASS()
+class SPECTRE_API AEMFReader : public ASwitchable
+{
+	GENERATED_BODY()
+	
+};
