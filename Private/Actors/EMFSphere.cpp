f262bd5b03f06e4b59ec45feec486bdabe3114f6
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-11-17 13:07:51 +0000

    - Sets up collision component in constructor
    - Set emf events to have a lifetime
    
    - draws debug sphere to show how the emf events look

--- Private/Actors/EMFSphere.cpp
+++ Private/Actors/EMFSphere.cpp
@@ -1,14 +1,27 @@
 
 #include "Actors/EMFSphere.h"
 
+#include "Components/CapsuleComponent.h"
+#include "DrawDebugHelpers.h"
+
 AEMFSphere::AEMFSphere()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	CollisionCapsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collision Capsule"));
 }
 
 void AEMFSphere::BeginPlay()
 {
 	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(
+		DestructHandle,
+		this,
+		&AEMFSphere::OnDestruct,
+		Lifetime,
+		false
+		);
 }
 
 void AEMFSphere::Initialise(int32 EMF_Level, float EMF_Radius, FVector EMF_Center)
@@ -18,10 +31,16 @@ void AEMFSphere::Initialise(int32 EMF_Level, float EMF_Radius, FVector EMF_Cente
 	Center = EMF_Center;
 
 	this->SetActorLocation(Center);
+
+	if (CollisionCapsule)
+	{
+		CollisionCapsule->SetCapsuleSize(EMF_Radius, EMF_Radius, true);
+	}
+
+	DrawDebugSphere(GetWorld(), EMF_Center, EMF_Radius, 16, FColor::Red, false, Lifetime);
 }
 
-void AEMFSphere::SpawnEMF()
+void AEMFSphere::OnDestruct()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Spawning EMF"));
+	this->Destroy();
 }
-
