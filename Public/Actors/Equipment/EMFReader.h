e012307e0d0ca43004e65a35c655d14630b196b4
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-11-20 10:39:20 +0000

    - Added overrides for TurnedOn and TurnedOff from ASwitchable
    - Added point lights as indicators for EMF

--- Public/Actors/Equipment/EMFReader.h
+++ Public/Actors/Equipment/EMFReader.h
@@ -5,10 +5,35 @@
 #include "Actors/Equipment/Switchable.h"
 #include "EMFReader.generated.h"
 
+class UPointLightComponent;
 
 UCLASS()
 class SPECTRE_API AEMFReader : public ASwitchable
 {
 	GENERATED_BODY()
+
+public:
+	AEMFReader();
 	
+	virtual void TurnedOn() override;
+
+	virtual void TurnedOff() override;
+
+	UPROPERTY(EditAnywhere)
+	UPointLightComponent* PowerIndicator;
+
+	UPROPERTY(EditAnywhere)
+	UPointLightComponent* Level_1_Indicator;
+
+	UPROPERTY(EditAnywhere)
+	UPointLightComponent* Level_2_Indicator;
+
+	UPROPERTY(EditAnywhere)
+	UPointLightComponent* Level_3_Indicator;
+
+	UPROPERTY(EditAnywhere)
+	UPointLightComponent* Level_4_Indicator;
+
+	UPROPERTY(EditAnywhere)
+	UPointLightComponent* Level_5_Indicator;
 };
