da71d57b4846c8297706becc640c9ae91878ff01
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-11-20 10:39:41 +0000

    - Setup point lights in the constructor

--- Private/Actors/Equipment/EMFReader.cpp
+++ Private/Actors/Equipment/EMFReader.cpp
@@ -1,3 +1,38 @@
 
 #include "Actors/Equipment/EMFReader.h"
 
+#include "Components/PointLightComponent.h"
+
+
+AEMFReader::AEMFReader()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	PowerIndicator = CreateDefaultSubobject<UPointLightComponent>(TEXT("Power Indicator"));
+	PowerIndicator->SetupAttachment(Mesh);
+
+	Level_1_Indicator = CreateDefaultSubobject<UPointLightComponent>(TEXT("EMF 1"));
+	Level_1_Indicator->SetupAttachment(Mesh);
+
+	Level_2_Indicator = CreateDefaultSubobject<UPointLightComponent>(TEXT("EMF 2"));
+	Level_2_Indicator->SetupAttachment(Mesh);
+
+	Level_3_Indicator = CreateDefaultSubobject<UPointLightComponent>(TEXT("EMF 3"));
+	Level_3_Indicator->SetupAttachment(Mesh);
+
+	Level_4_Indicator = CreateDefaultSubobject<UPointLightComponent>(TEXT("EMF 4"));
+	Level_4_Indicator->SetupAttachment(Mesh);
+
+	Level_5_Indicator = CreateDefaultSubobject<UPointLightComponent>(TEXT("EMF 5"));
+	Level_5_Indicator->SetupAttachment(Mesh);
+}
+
+void AEMFReader::TurnedOn()
+{
+	Super::TurnedOn();
+}
+
+void AEMFReader::TurnedOff()
+{
+	Super::TurnedOff();
+}
